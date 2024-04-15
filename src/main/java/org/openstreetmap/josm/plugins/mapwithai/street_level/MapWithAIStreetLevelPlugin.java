a0a4480e65c94171a6fa5b90b0aa9348b4bd3a67
Taylor Smock <tsmock@meta.com>
2024-04-15 13:06:02 -0600

    Remove legacy code
    Signed-off-by: Taylor Smock <tsmock@meta.com>

--- src/main/java/org/openstreetmap/josm/plugins/mapwithai/street_level/MapWithAIStreetLevelPlugin.java
+++ src/main/java/org/openstreetmap/josm/plugins/mapwithai/street_level/MapWithAIStreetLevelPlugin.java
@@ -7,9 +7,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import java.awt.Component;
-import java.util.Collection;
 
-import org.openstreetmap.josm.actions.ExtensionFileFilter;
 import org.openstreetmap.josm.data.UndoRedoHandler;
 import org.openstreetmap.josm.data.osm.AbstractPrimitive;
 import org.openstreetmap.josm.gui.IconToggleButton;
@@ -18,17 +16,12 @@ import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
-import org.openstreetmap.josm.plugins.mapwithai.street_level.actions.ApplySuggestionAction;
-import org.openstreetmap.josm.plugins.mapwithai.street_level.actions.MapWithAIStreetLevelAction;
 import org.openstreetmap.josm.plugins.mapwithai.street_level.actions.ParallelSidewalkCreationAction;
 import org.openstreetmap.josm.plugins.mapwithai.street_level.actions.mapmode.SidewalkMode;
 import org.openstreetmap.josm.plugins.mapwithai.street_level.data.CrossingCommandListener;
 import org.openstreetmap.josm.plugins.mapwithai.street_level.data.preferences.MapWithAIStreetLevelConfig;
 import org.openstreetmap.josm.plugins.mapwithai.street_level.data.preferences.MapWithAIStreetLevelUrls;
-import org.openstreetmap.josm.plugins.mapwithai.street_level.gui.io.importexport.CubitorOsmChangeImporter;
 import org.openstreetmap.josm.tools.Destroyable;
-import org.openstreetmap.josm.tools.JosmRuntimeException;
-import org.openstreetmap.josm.tools.ReflectionUtils;
 
 /**
  * The POJO for StreetLevel AI detections
@@ -46,12 +39,9 @@ public class MapWithAIStreetLevelPlugin extends Plugin implements Destroyable {
     public MapWithAIStreetLevelPlugin(PluginInformation info) {
         super(info);
         final JMenu dataMenu = MainApplication.getMenu().dataMenu;
-        MainMenu.add(dataMenu, new MapWithAIStreetLevelAction());
-        MainMenu.add(dataMenu, new ApplySuggestionAction());
         MainMenu.add(dataMenu, new ParallelSidewalkCreationAction());
         MapWithAIStreetLevelConfig.setUrls(new MapWithAIStreetLevelUrls());
         AbstractPrimitive.getDiscardableKeys().add("suggestion-id");
-        ExtensionFileFilter.addImporterFirst(CubitorOsmChangeImporter.INSTANCE);
     }
 
     @Override
@@ -74,24 +64,6 @@ public class MapWithAIStreetLevelPlugin extends Plugin implements Destroyable {
                     .getPackage().getName().contains(this.getClass().getPackage().getName())) {
                 dataMenu.remove(jMenu);
             }
-        }
-        removeImporter();
-    }
-
-    /**
-     * Remove the importer we added (uses reflection, unfortunately)
-     */
-    private static void removeImporter() {
-        // FIXME: Add method in JOSM to remove importer
-        try {
-            final var importersField = ExtensionFileFilter.class.getDeclaredField("importers");
-            ReflectionUtils.setObjectsAccessible(importersField);
-            final Object importers = importersField.get(null);
-            if (importers instanceof Collection) {
-                ((Collection<?>) importers).remove(CubitorOsmChangeImporter.INSTANCE);
-            }
-        } catch (ReflectiveOperationException reflectiveOperationException) {
-            throw new JosmRuntimeException(reflectiveOperationException);
         }
     }
 }
